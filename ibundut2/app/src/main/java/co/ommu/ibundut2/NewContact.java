2e2debaf44b041d8576d30392856b25a46897adb
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2016-01-20 21:43:46 +0700

    ibundut: employe contact

---
+++ ibundut2/app/src/main/java/co/ommu/ibundut2/NewContact.java
@@ -0,0 +1,142 @@
+package co.ommu.ibundut2;
+
+import android.app.Activity;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+public class NewContact extends Activity {
+
+	private static final String EMPTY_STRING = "";
+	private Button NewContact_BTNSave;
+	private Spinner NewContact_Department;
+
+	private EditText NewContact_FirstName;
+	private EditText NewContact_LastName;
+	private EditText NewContact_Title;
+	private EditText NewContact_OfficePhone;
+	private EditText NewContact_OfficePhoneExt;
+	private EditText NewContact_CellPhone;
+	private EditText NewContact_Email;
+	private EditText NewContact_AIM;
+	private EditText NewContact_MSN;
+	private CheckBox NewContact_isManager;
+	private String defaultOfficePhone = "703.689.2121";
+
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.new_contact);
+	}
+
+	// NewContact_BTNSave onClick handler
+	public void newContactSave(View button) {
+		this.findAllViewsById();
+
+		String firstName = NewContact_FirstName.getText().toString();
+		String lastName = NewContact_LastName.getText().toString();
+		String title = NewContact_Title.getText().toString();
+		String officePhone = NewContact_OfficePhone.getText().toString();
+		String officePhoneExt = NewContact_OfficePhoneExt.getText().toString();
+		String cellPhone = NewContact_CellPhone.getText().toString();
+		String email = NewContact_Email.getText().toString();
+		String aim = NewContact_AIM.getText().toString();
+		String msn = NewContact_MSN.getText().toString();
+		boolean isManager = NewContact_isManager.isChecked();
+		String department = NewContact_Department.getSelectedItem().toString();
+
+		if (officePhone.length() < 2) { officePhone = defaultOfficePhone; }
+		
+		// check to see if user exists
+		int employeeId = 0;
+
+		SQLiteDatabase db = (new DatabaseHelper(this)).getWritableDatabase();
+		Cursor cursor = db
+				.rawQuery(
+						"SELECT _id FROM viridEmployee WHERE firstName = ? and lastName = ?",
+						new String[] { firstName, lastName });
+
+		// USER EXISTS SHOW MESSAGE
+		if (cursor.getCount() == 1) {
+			alert("User already exists.", Toast.LENGTH_LONG);
+		}
+
+		// USER NOT FOUND, PROCEED TO CREATE RECORD
+		else {
+
+			int isManagerInt = 0;
+			if (isManager == true) { isManagerInt = 1; }
+			
+			
+			String sql = "INSERT INTO viridEmployee (firstName, lastName, title, department, isManager, managerId, officePhone, officePhoneExt, cellPhone, email, aim, msn) VALUES ('"
+					+ firstName
+					+ "', '"
+					+ lastName
+					+ "', '"
+					+ title
+					+ "', '"
+					+ department
+					+ "', '"
+					+ isManagerInt
+					+ "', '0', '"
+					+ officePhone
+					+ "', '"
+					+ officePhoneExt
+					+ "', '"
+					+ cellPhone
+					+ "', '"
+					+ email
+					+ "', '"
+					+ aim
+					+ "', '"
+					+ msn + "')";
+			db.execSQL(sql);
+
+			if (wasAdded(firstName, lastName)) {
+				alert("User added.", Toast.LENGTH_LONG);
+			} else {
+				alert("Error adding user.", Toast.LENGTH_LONG);
+			}
+		}
+
+		// add to database
+	}
+
+	public void alert(String message, int length) {
+		Toast.makeText(getApplicationContext(), message, length).show();
+	}
+
+	public boolean wasAdded(String firstName, String lastName) {
+		SQLiteDatabase db = (new DatabaseHelper(this)).getWritableDatabase();
+		Cursor cursor = db
+				.rawQuery(
+						"SELECT _id FROM viridEmployee WHERE firstName = ? and lastName = ?",
+						new String[] { firstName, lastName });
+		if (cursor.getCount() > 0) {
+			return true;
+		}
+
+		return false;
+	}
+
+	private void findAllViewsById() {
+		NewContact_FirstName = (EditText) findViewById(R.id.NewContact_FirstName);
+		NewContact_LastName = (EditText) findViewById(R.id.NewContact_LastName);
+		NewContact_Title = (EditText) findViewById(R.id.NewContact_Title);
+		NewContact_OfficePhone = (EditText) findViewById(R.id.NewContact_OfficePhone);
+		NewContact_OfficePhoneExt = (EditText) findViewById(R.id.NewContact_OfficePhoneExt);
+		NewContact_CellPhone = (EditText) findViewById(R.id.NewContact_CellPhone);
+		NewContact_Email = (EditText) findViewById(R.id.NewContact_Email);
+		NewContact_AIM = (EditText) findViewById(R.id.NewContact_AIM);
+		NewContact_MSN = (EditText) findViewById(R.id.NewContact_MSN);
+		NewContact_Department = (Spinner) findViewById(R.id.NewContact_Department);
+		NewContact_isManager = (CheckBox) findViewById(R.id.NewContact_isManager);
+		NewContact_BTNSave = (Button) findViewById(R.id.NewContact_BTNSave);
+	}
+
+}
---
+++ ibundut2/app/src/main/java/co/ommu/ibundut2/ViridDirectoryActivity.java
@@ -0,0 +1,172 @@
+package co.ommu.ibundut2;
+
+import android.app.Dialog;
+import android.app.ListActivity;
+import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.os.Bundle;
+import android.os.Handler;
+import android.text.Html;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ListAdapter;
+import android.widget.ListView;
+import android.widget.SimpleCursorAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class ViridDirectoryActivity extends ListActivity {
+
+	private static final int MENU_NEW_CONTACT = 0;
+	private static final int MENU_HELP = 1;
+	private static final int MENU_WIPEDB = 2;
+	protected EditText searchText;
+	protected SQLiteDatabase db;
+	protected Cursor cursor;
+	protected ListAdapter adapter;
+	protected Dialog mSplashDialog;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+
+		MyStateSaver data = (MyStateSaver) getLastNonConfigurationInstance();
+		if (data != null) {
+			setContentView(R.layout.main);
+			searchText = (EditText) findViewById(R.id.searchText);
+			db = (new DatabaseHelper(this)).getWritableDatabase();
+
+			// Rebuild your UI with your saved state here
+		} else {
+			setContentView(R.layout.main);
+			searchText = (EditText) findViewById(R.id.searchText);
+			db = (new DatabaseHelper(this)).getWritableDatabase();
+			// Do your heavy loading here on a background thread
+		}
+		getListData();
+	}
+
+	
+	//	MENUING OPTIONS --------------
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		menu.add(0, MENU_NEW_CONTACT, 0, "New Contact").setIcon(R.mipmap.add);
+		menu.add(0, MENU_HELP, 1, "Help").setIcon(R.mipmap.help);
+		menu.add(0, MENU_WIPEDB, 2, "Clear Database");
+		return true;
+	}
+
+	public boolean onOptionsItemSelected(MenuItem item) {
+		
+		Intent intent;
+		
+		switch (item.getItemId()) {
+			case MENU_NEW_CONTACT:
+				intent = new Intent(this, NewContact.class);
+				startActivity(intent);
+				break;
+//			case MENU_HELP:
+//				intent = new Intent(this, Help.class);
+//				startActivity(intent);
+//				break;
+			case MENU_WIPEDB:
+				wipeDatabase();
+				break;
+		}
+		return false;
+	}
+	// -------------------------------
+	
+	
+	public void onListItemClick(ListView parent, View view, int position,
+			long id) {
+		Intent intent = new Intent(this, EmployeeDetails.class);
+		Cursor cursor = (Cursor) adapter.getItem(position);
+		intent.putExtra("EMPLOYEE_ID",
+				cursor.getInt(cursor.getColumnIndex("_id")));
+		startActivity(intent);
+	}
+
+	public void search(View view) {
+		// || is the concatenation operation in SQLite
+		getListData();
+	}
+
+	private void getListData() {
+		cursor = db.rawQuery(
+				"SELECT _id, firstName, lastName, title, \" - \" || department as department FROM viridEmployee WHERE firstName || ' ' || lastName LIKE ?",
+				new String[]{"%" + searchText.getText().toString()
+								+ "%" });
+		adapter = new SimpleCursorAdapter(this, R.layout.employee_list_item,
+				cursor, new String[] { "firstName", "lastName", "title",
+				"department" }, new int[] { R.id.firstName,
+				R.id.lastName, R.id.title, R.id.department });
+		setListAdapter(adapter);
+	}
+
+	@Override
+	public Object onRetainNonConfigurationInstance() {
+		MyStateSaver data = new MyStateSaver();
+		// Save your important data here
+
+		if (mSplashDialog != null) {
+			data.showSplashScreen = true;
+			removeSplashScreen();
+
+		}
+		return data;
+	}
+
+	/**
+	 * Removes the Dialog that displays the splash screen
+	 */
+	protected void removeSplashScreen() {
+		if (mSplashDialog != null) {
+			mSplashDialog.dismiss();
+			mSplashDialog = null;
+		}
+	}
+
+	/**
+	 * Shows the splash screen over the full Activity
+	 */
+	protected void showSplashScreen() {
+		mSplashDialog = new Dialog(this, R.style.SplashScreen);
+		mSplashDialog.setContentView(R.layout.splash);
+		mSplashDialog.setCancelable(false);
+		mSplashDialog.show();
+
+		TextView splashText = (TextView) mSplashDialog
+				.findViewById(R.id.splashText);
+		splashText.setText(Html.fromHtml(getString(R.string.splashText)));
+
+		// Set Runnable to remove splash screen just in case
+		final Handler handler = new Handler();
+		handler.postDelayed(new Runnable() {
+			public void run() {
+				removeSplashScreen();
+			}
+		}, 3000);
+	}
+
+	/**
+	 * Simple class for storing important data across config changes
+	 */
+	private class MyStateSaver {
+		public boolean showSplashScreen = false;
+		// Your other important fields here
+	}
+	
+	
+	
+	private void wipeDatabase() {		
+		db.delete("viridEmployee", null, null);
+		Toast.makeText(getApplicationContext(), "Database cleared.", Toast.LENGTH_LONG);
+	}
+	
+	
+
+}
